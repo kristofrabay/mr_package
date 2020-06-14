ef5d0a092983bb11cc93a29a1328fd3b24b5c2d6
kristofrabay <kristof.rabay@gmail.com>
2020-06-14 14:35:23 +0000

    Ft to HUF ending

--- R/functions.R
+++ R/functions.R
@@ -20,10 +20,7 @@ get_bitcoin_price <- function(retried = 0) {
 #' @export
 #' @param x number
 #' @importFrom scales dollar
-<<<<<<< HEAD
 forint <- function(x) {dollar(x, suffix = " HUF", prefix = NULL)}
-=======
-forint <- function(x) {dollar(x, suffix = "Ft", prefix = NULL)}
 
 
 
